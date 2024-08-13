0de000aa68a931e3ccdd402161fa7e528f599a61
Clemond <quackshows11@gmail.com>
2024-08-13 16:13:24 +0200

    Added a more refined header design with text that has different sizes

--- Components/Header.js
+++ Components/Header.js
@@ -3,10 +3,9 @@ import { View, StyleSheet, Text } from "react-native";
 export default function Header() {
   return (
     <View style={styles.header}>
-      <Text style={styles.headerText}>
-        Welcome to the Rock Paper Scissors Game
-      </Text>
-      <Text style={styles.headerText}>Pick an item and start playing</Text>
+      <Text style={styles.headerTextLarge}>Welcome to</Text>
+      <Text style={styles.headerTextMedium}>The Rock Paper Scissors Game</Text>
+      <Text style={styles.headerTextSmall}>Pick an item and start playing</Text>
     </View>
   );
 }
@@ -15,20 +14,46 @@ export default function Header() {
 
 const styles = StyleSheet.create({
   header: {
-    //backgroundColor: "#556B2F",
-    //borderWidth: 1.5,
-    //borderBottomLeftRadius: 8,
-    // borderBottomRightRadius: 8,
-    marginTop: 30,
+    marginTop: 45,
+    marginHorizontal: 10,
     height: 100,
     justifyContent: "center",
     alignItems: "center",
   },
-  headerText: {
+  headerTextSmall: {
     fontWeight: "bold",
     fontStyle: "italic",
     fontSize: 20,
     color: "white",
+
+    // Shadow properties for text
+    textShadowColor: "#000",
+    textShadowOffset: { width: 0, height: 1 },
+    textShadowRadius: 10,
+
+    // Adding a bit of elevation on Android (if needed)
+    elevation: 5,
+  },
+  headerTextLarge: {
+    fontWeight: "bold",
+    fontStyle: "italic",
+    fontSize: 28,
+    color: "white",
+
+    // Shadow properties for text
+    textShadowColor: "#000",
+    textShadowOffset: { width: 0, height: 1 },
+    textShadowRadius: 10,
+
+    // Adding a bit of elevation on Android (if needed)
+    elevation: 5,
+  },
+  headerTextMedium: {
+    fontWeight: "bold",
+    fontStyle: "italic",
+    fontSize: 22,
+    color: "white",
+
     // Shadow properties for text
     textShadowColor: "#000",
     textShadowOffset: { width: 0, height: 1 },
